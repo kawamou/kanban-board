9ce0f20c7c2a2ff6b46205eca4ab90ecc1b9def0
kawamou <kwmkzsd@gmail.com>
2022-06-28 09:54:27 +0900

    NewColumnButtonコンポーネントを追加

---
+++ src/components/NewColumnButton.tsx
@@ -0,0 +1,21 @@
+import { PlusIcon } from "@heroicons/react/outline";
+
+type NewColumnButtonProps = {
+  updateShowModal: (showModal: boolean) => void;
+};
+
+export const NewColumnButton = (props: NewColumnButtonProps) => {
+  return (
+    <button
+      className="border-dashed border-x border-y boder-t border-b w-[335px] h-[103px] hover:underline"
+      onClick={() => {
+        props.updateShowModal(true);
+      }}
+    >
+      <div className="flex items-center justify-center">
+        <PlusIcon className="w-4 h-4"></PlusIcon>
+        <span className="ml-2">Add column</span>
+      </div>
+    </button>
+  );
+};
