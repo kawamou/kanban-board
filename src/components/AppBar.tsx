93a54c0f30730072d24b22b78a4577a73de3b110
kawamou <kwmkzsd@gmail.com>
2022-06-28 09:53:34 +0900

    AppBarコンポーネントを追加

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { MenuIcon } from "@heroicons/react/outline";
+
+const AppBar = () => {
+  return (
+    <div className="flex items-center h-14 bg-[#24292f]">
+      <button>
+        <MenuIcon className="ml-4 text-gray-50 h-6 w-6"></MenuIcon>
+      </button>
+      <span className="ml-4 text-gray-50 ">react-kanban-sample</span>
+    </div>
+  );
+};
+
+export default AppBar;
