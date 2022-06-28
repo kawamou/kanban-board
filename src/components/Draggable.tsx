136e135d087d46b754fcb2d82285138b06f38c9b
kawamou <kwmkzsd@gmail.com>
2022-06-28 09:54:15 +0900

    Draggableコンポーネントを追加

---
+++ src/components/Draggable.tsx
@@ -0,0 +1,70 @@
+import { FC } from "react";
+import { Item, ItemWithIndex } from "../item";
+import { useDrag, useDrop } from "react-dnd";
+import { useRef } from "react";
+
+export const ItemType = ["item" as const];
+
+type Props = {
+  item: Item;
+  index: number;
+  move: (dragIndex: number, hoverIndex: number, groupName: string) => void;
+  children: React.ReactNode;
+};
+
+export const Draggable: FC<Props> = ({
+  item: item,
+  index,
+  move: move,
+  children,
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [, drop] = useDrop({
+    accept: item.type,
+    hover(dragItem: ItemWithIndex, monitor) {
+      if (!ref.current) return;
+
+      const dragIndex = dragItem.index;
+      const hoverIndex = index;
+
+      if (dragIndex === hoverIndex) return;
+
+      if (dragIndex === undefined) return;
+
+      if (item.groupName === dragItem.groupName) {
+        // グループ内での並び替えの場合は入れ替え方向とhover位置に応じて入れ替えるかを確定
+        const hoverRect = ref.current.getBoundingClientRect();
+        const hoverMiddleY = (hoverRect.bottom - hoverRect.top) / 2;
+        const mousePosition = monitor.getClientOffset();
+        if (!mousePosition) return;
+        const hoverClientY = mousePosition.y - hoverRect.top;
+        if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY * 0.5) return;
+        if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY * 1.5) return;
+      }
+
+      move(dragIndex, hoverIndex, item.groupName);
+
+      dragItem.index = hoverIndex;
+      dragItem.groupName = item.groupName;
+    },
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    type: item.type,
+    item: { ...item, index },
+    isDragging: (monitor) => monitor.getItem().key === item.key,
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+      canDrag: monitor.canDrag(),
+    }),
+  });
+
+  drag(drop(ref));
+
+  return (
+    <div style={{ opacity: isDragging ? 0 : 1 }} ref={ref} className="h-full">
+      {children}
+    </div>
+  );
+};
